c7331aa2b1a20cd311657a785649ede7654c491d
AmauryTavares <amaury.tavares@ufrpe.br>
2020-10-11 00:01:10 -0300

    Creation of player movement and generation of tilemap

---
+++ RUnners/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 direction;
+    public float speed;
+
+    private int desiredLane = 1; // 0 = left, 1 = middle, 2 = right
+    public float laneDistance; // Distance between each two lanes
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.y = speed;
+        HandlerInputKeyboard();
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(direction * Time.deltaTime);// Update movement forward
+    }
+
+    private void HandlerInputKeyboard()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))// Handle input right
+        {
+            desiredLane++;
+
+            if (desiredLane == 3)
+            {
+                desiredLane = 2;
+            }
+
+        } else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))// Handle input left
+        {
+            desiredLane--;
+
+            if (desiredLane == -1)
+            {
+                desiredLane = 0;
+            }
+        }
+
+        CalculateNextPosition(desiredLane);
+    }
+
+    private void CalculateNextPosition(int desiredLane) //Calculate and update next position
+    {
+        Vector3 targetPosition = transform.position.y * transform.right;
+        
+        if (desiredLane == 0) // Update target position to left
+        {
+            targetPosition += Vector3.left * laneDistance;
+        }
+        else if (desiredLane == 2) // Update target position to right
+        {
+            targetPosition += Vector3.right * laneDistance;
+        }
+
+        if (transform.position == targetPosition)
+        {
+            return;
+        } else
+        {
+            Vector3 diff = targetPosition - transform.position;
+            Vector3 moverDir = diff.normalized * 25 * Time.deltaTime;
+
+            if (moverDir.sqrMagnitude < diff.sqrMagnitude)
+            {
+                controller.Move(moverDir);
+            } else
+            {
+                controller.Move(diff);
+            }
+        }
+    }
+
+}
---
+++ RUnners/Assets/Scripts/TileManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+
+    public GameObject[] tilePrefabs;
+    public float ySpawn;
+    public float tileLength;
+    public int numberOfTiles;
+
+    private List<GameObject> activeTiles = new List<GameObject>();
+
+    public Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {  
+        for (int i = 0; i < numberOfTiles; i++) // Spawn initials tiles
+        {
+            if (i == 0)
+            {
+                SpawnTile(0);
+            } else
+            {
+                SpawnTile(Random.Range(0, tilePrefabs.Length));
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform.position.y - tileLength > ySpawn - (numberOfTiles * tileLength)) // when the last tile is passed spawn new tile
+        {
+            SpawnTile(0);
+            DeleteTile();
+        }
+    }
+
+    private void SpawnTile(int tileIndex) // Spawn new tile
+    {
+        GameObject go = Instantiate(tilePrefabs[tileIndex], transform.up * ySpawn, transform.rotation);
+        activeTiles.Add(go);
+
+        ySpawn += tileLength;
+    } 
+
+    private void DeleteTile() // Remove last Tile
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}
